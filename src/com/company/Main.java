476c6fa61f477c5fda173c263ad2b936ac08ee32
Mateusz Tarchala <Janusz@kowalski.com>
2022-02-23 11:02:52 +0100

    Fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,5 @@ import java.util.Scanner;
 //Zmieniony ignore
 class Main
 {
-    public static void main(String arg[])
-    {
-        int n;
-        Scanner sc=new Scanner(System.in);
 
-        System.out.println("Enter a number :");
-        n=sc.nextInt();
-
-        System.out.println("Add Up To("+n+") ----->"+addUpTo(n));
-    }
-    static int addUpTo(int n)
-    {
-        int sum=0;
-        for(int i=1;i<=n;i++)
-        {
-            sum=sum+i;
-        }
-        return sum;
-    }
 }
